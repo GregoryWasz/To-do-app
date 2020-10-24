3bf13f083fdbc4f8faf035e0af5243ab7c0abe6c
GregWasz <gregory.waszkow@icloud.com>
2020-10-24 19:34:22 +0200

    Added routing

--- todoapp-react-frontend/src/App.js
+++ todoapp-react-frontend/src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import FooterComponent from './compontens/FooterComponent';
 import HeaderComponent from './compontens/HeaderComponent';
 import ListTaskComponent from './compontens/ListTaskComponent';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-          <div><ListTaskComponent /></div>
-      </div>
-      <FooterComponent />
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Switch> https://localhost:3000/
+              <Route path="/" component={ListTaskComponent}></Route>
+              <Route path="/tasks" component={ListTaskComponent}></Route>
+              <ListTaskComponent />
+            </Switch>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
