850f01f622ed4db0597ea592614b5e80bda04388
GregWasz <gregory.waszkow@icloud.com>
2020-10-25 11:05:21 +0100

    Added getTask and updateTask

--- todoapp/src/main/java/com/paw2/todoapp/controller/TaskController.java
+++ todoapp/src/main/java/com/paw2/todoapp/controller/TaskController.java
@@ -1,9 +1,11 @@
 package com.paw2.ToDoApp.controller;
 
 
+import com.paw2.ToDoApp.exception.ResourceNotFoundException;
 import com.paw2.ToDoApp.model.Task;
 import com.paw2.ToDoApp.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,4 +31,21 @@ public class TaskController {
         return taskRepository.save(task);
     }
 
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable Long id){
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not exist with id: " + id));
+        return ResponseEntity.ok(task);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id,@RequestBody Task taskDetails){
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not exist with id: " + id));
+        task.setNote(taskDetails.getNote());
+        task.setText(taskDetails.getText());
+
+        Task updatedTask = taskRepository.save(task);
+        return ResponseEntity.ok(updatedTask);
+    }
 }
